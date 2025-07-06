c5c292c65753049f73e423f86a95bfe3c7959f0f
Umer21K <m.umernasir7017@gmail.com>
2025-07-06 21:11:31 +0500

    chore: sync package-lock.json with package.json

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,14 +3,11 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -30,35 +27,35 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>I'm a passionate and versatile tech enthusiast with a strong foundation in full-stack development, cloud computing, and AI/ML Integration.<br></br>Whether it's troubleshooting deployment errors, connecting frontend apps to backends securely, or creating unique visual branding, I strive to deliver robust and innovative solutions.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>App Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>AI/ML integrations</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>I'm a passionate and versatile tech enthusiast with a strong foundation in full-stack development, cloud computing, and AI/ML Integration.<br />Whether it's troubleshooting deployment errors, connecting frontend apps to backends securely, or creating unique visual branding, I strive to deliver robust and innovative solutions.</p>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>App Development</h5>
                 </div>
+                <div className="item">
+                  <img src={meter2} alt="" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="" />
+                  <h5>AI/ML integrations</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="" />
+                  <h5>Web Development</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="" />
     </section>
-  )
-}
+  );
+};
